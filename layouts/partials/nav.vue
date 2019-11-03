919ab3f62bc3cc2f3ba620569e517f2a6da06850
AlenaNik <alena.nik.digital@gmail.com>
2019-11-03 19:42:20 +0100

    added layout, again..

---
+++ layouts/partials/nav.vue
@@ -0,0 +1,78 @@
+<template>
+  <header>
+    <nav class="navbar fixed min-h-screen flex justify-center items-center mx-2">
+      <ul>
+        <li
+          class="mb-10 mx-6 sm:mx-6 xs:mx-6"
+        ><nuxt-link
+          to="/">Home
+        </nuxt-link>
+        </li>
+        <li
+          class="mb-10 mx-6"
+        ><nuxt-link
+          to="/work">Work</nuxt-link></li>
+        <li
+          class="mb-10 mx-6"
+        ><nuxt-link
+          to="/contact">Contact</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Nav'
+  }
+</script>
+
+<style scoped>
+  .navbar {
+    font-family: "Helvetica Neue";
+    font-size: 20px;
+    font-weight: 700;
+    color: black;
+  }
+  a {
+    font-size: 16px;
+    position: relative;
+    text-decoration: none;
+    padding-bottom: 8px;
+
+    &:before,
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 2px;
+      left: 0; right: 0;
+      height: 2px;
+      background-color: #5967d8;
+    }
+    &:before {
+      opacity: 0;
+      transform: translateY(-8px);
+      transition: transform 0s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity 0s;
+    }
+    &:after {
+      opacity: 0;
+      transform: translateY(4);
+      transition: transform .6s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity .2s;
+    }
+    &:hover,
+    &:focus {
+      &:before,
+      &:after {
+        opacity: 1;
+        transform: translateY(0);
+      }
+      &:before {
+        transition: transform .6s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity .2s;
+      }
+      &:after {
+        transition: transform 0s .2s cubic-bezier(0.175, 0.885, 0.320, 1.275), opacity 0s .2s;
+      }
+    }
+  }
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,72 @@
+
+export default {
+  mode: 'universal',
+  generate: {
+    fallback: true
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Alena Website',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Alena Nikolaeva portfolio website' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: '~/components/Loading.vue',
+  /*
+  ** Global CSS
+  */
+  css: [
+    'assets/css/tailwind.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    [
+      'nuxt-fontawesome', {
+      imports: [
+        {
+          set: '@fortawesome/free-solid-svg-icons',
+          icons: ['fas']
+        },
+        {
+          set:'@fortawesome/free-brands-svg-icons',
+          icons: ['fab']
+        }
+      ]
+    }
+    ]
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
